a22470c008754da6855eb781d11d6f6ccd370bcb
Derin Ozturk <ddozturk7@gmail.com>
2018-04-24 17:40:14 -0400

    Merged Human Detect in to camera.py

--- camera.py
+++ camera.py
@@ -4,6 +4,10 @@ from picamera.array import PiRGBArray
 import cv2
 
 from imutils import contours
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import argparse
+
 from skimage import measure
 import numpy as np
 import imutils
@@ -90,6 +94,35 @@ class Camera():
 
         cv2.imshow("Image", im)
         cv2.waitKey(0)
+
+    def detectHuman(self):
+        hog = cv2.HOGDescriptor()
+        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+        im = self.image
+        im = imutils.resize(im, width=min(400, im.shape[1]))
+        orig = im.copy()
+
+        #detect peope in the image
+        (rects, weights) = hog.detectMultiScale(im, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        
+        # draw the original bounding boxes
+        for (x, y, w, h) in rects:
+            cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        # apply non-maxima suppression to the bounding boxes using a fiarly large overlap threshold to try to maintain overlapping
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+        # draw final bounding boxes
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(im, (xA, yA), (xB, yB), (0, 255,0), 2)
+
+        # show images
+        cv2.imshow("Before", orig)
+        cv2.imshow("After", im)
+        cv2.waitKey(0)  
+
 
 
 if __name__ == '__main__':
