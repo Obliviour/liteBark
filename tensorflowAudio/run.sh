0b5f8db3a9c36fe6494131da498c6445dceed201
Obliviour <victor.barr@yahoo.com>
2018-04-25 21:32:42 +0000

    Victor actually did stuff

--- tensorflowAudio/run.sh
+++ tensorflowAudio/run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 python wav_trigger_inference.py \
---graph=/home/pi/tensorflowAudio/speech_commands/conv_actions_frozen.pb \
---labels=/home/pi/tensorflowAudio/speech_commands/conv_actions_labels.txt \
+--graph=./speech_commands/conv_actions_frozen.pb \
+--labels=./speech_commands/conv_actions_labels.txt \
 --wav=./file.wav 
